8976a1ce0219cf513fc28c21ef7803a82056499d
Janaki07 <133958221+Janaki07@users.noreply.github.com>
2023-05-20 18:38:13 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,14 @@
 package com.library.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
+import com.library.dao.BookDAO;
 import com.library.model.Book;
+import com.library.model.Member;
 
 public class test {
 
@@ -11,19 +17,40 @@ public class test {
 		Scanner sc = new Scanner(System.in);
 		
      Book b = new Book();
-     System.out.println("Enter book id");
-     int bid=sc.nextInt();
-     b.setBookid(bid);
-     System.out.println("Enter book name");
-     String bname=sc.next();
-     b.setBookname(bname);
-     System.out.println("Enter book author");
-     String author = sc.next();
-     b.setAuthor(author);
-     System.out.println("Enter book availability");
-      String avilability = sc.next();
-      b.setAvailability(avilability);
+     Member m= new Member();
+     BookDAO bd = new BookDAO();
+     Map<Book, Member> bookMemberMap = new HashMap<>();
+    System.out.println("Enter book name : ");
+    String bname=sc.next();
+    b.setBookname(bname);
+    System.out.println("Enter book id : ");
+    int bid=sc.nextInt();
+    b.setBookid(bid);
+    System.out.println("Enter book author : ");
+    String author = sc.next();
+b.setAuthor(author);
+    System.out.println("Enter book avilability status : ");
+    String avilability = sc.next();
+    b.setAvailability(avilability);
+
+     System.out.println("Enter member id");
+     int mid=sc.nextInt();
+     m.setMemberId(mid);
+     System.out.println("Enter member name");
+     String mname=sc.next();
+     m.setMemberName(mname);
+     System.out.println("Enter bookborrowed");
+     int bookborrowed = sc.nextInt();
+     m.setBookborrowedid(bookborrowed);
+     System.out.println("Enter returnstatus");
+      String returnstatus = sc.next();
+      m.setReturnstatus(returnstatus);
      System.out.println(b);
+     List<Book> bookList = new ArrayList<>();
+     bookList.add(b);
+     List<Member> MemberList = new ArrayList<>();
+     MemberList.add(m);
+     bd.addMember(bookList,MemberList );
 	}
 
 }
